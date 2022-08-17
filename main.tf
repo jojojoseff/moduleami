b8010df07dcb3ba8257c1a87d56ed5a420ece604
jojo <jojojoseff@gmail.com>
2022-08-18 01:03:00 +0530

    module

---
+++ main.tf
@@ -0,0 +1,13 @@
+data "aws_ami" "example" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-*-gp2"]
+  }
+
+
+
+
+}
---
+++ output.tf
@@ -0,0 +1,5 @@
+output "latestami" {
+
+value = data.aws_ami.example.id
+
+}
